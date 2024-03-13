0e3438e50af4b04ca254af443a98492baa6b4301
Michel Chieregato <michel.chieregato@gmail.com>
2024-03-13 07:13:09 -0300

    Create exercicio_2.py

---
+++ exercicio_2.py
@@ -0,0 +1,16 @@
+"""
+#### Exercício 2
+
+Receba 2 inputs do usuário: O preço de um produto em reais e uma porcentagem de desconto a ser aplicada sobre ele (entre 0 e 100). Devolva o preço final do produto.
+
+Exemplo:
+
+Digite o preço do produtos em reais: 10.5
+
+Digite o desconto a ser aplicado (em porcentagem): 50
+
+
+Resposta:
+
+O preço final do produto é 5.25
+"""
