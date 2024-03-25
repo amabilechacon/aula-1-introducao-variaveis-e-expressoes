3875bb5478591a9a77b54dd12f6e11751a3f36d5
amabilechacon <60405498+amabilechacon@users.noreply.github.com>
2024-03-25 20:59:51 -0300

    Update exercicio_1.py

--- exercicio_1.py
+++ exercicio_1.py
@@ -13,3 +13,16 @@ O sucessor do número 4 é 5.
 O dobro do número 4 é 8.
 A raiz quadrada do número 4 é 2.
 """
+numero=int(input('digite seu numero:'))
+
+antecessor=numero-1
+sucessor=numero+1
+dobro=numero*2
+raiz=numero**(1/2)
+
+print(f'0 antecessor do numero {numero} é {antecessor}')
+print(f'0 sucessor do numero {numero} é {sucessor}')
+print(f'O dobro do número {numero} é {dobro}')
+print(f'A raiz do numero{numero} é {raiz}')
+
+print(f'O antecessor do número {numero} é {antecessor}')
